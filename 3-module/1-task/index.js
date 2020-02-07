317b8a42095b48c368a8989ce26c210766d9db06
unknown <aasolovkin@gmail.com>
2020-02-07 17:48:48 +0300

    Решение задачи 1 занятия 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,8 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    return data
+        .filter((person) => person.age <= age)
+        .map((person) => `${person.name}, ${person.balance}`)
+        .join('\n');
 }
