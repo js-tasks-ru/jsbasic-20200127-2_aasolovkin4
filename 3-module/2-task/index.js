b0c0e766d800affead293222bcaabd32c5c4a8b1
unknown <aasolovkin@gmail.com>
2020-02-07 18:16:05 +0300

    Решение задачи 2 занятия 3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,16 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
-}
+function getMinMax(string) {
+    let values = string
+          .split(' ')
+          .join(',')
+          .split(',')
+          .map(item => parseFloat(item))
+          .filter(item => !isNaN(item))
+          .sort((a, b) => a-b);
+      
+    return values.length === 0
+      ? { min: undefined, max: undefined }
+      : { min: values[0], max: values[values.length-1] }
+  }
