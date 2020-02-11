c4b62beaf44928f5f9a58223bdea5b9a75416244
unknown <aasolovkin@gmail.com>
2020-02-11 17:29:46 +0300

    Решение задачи 1 занятия 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,15 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+    let friendsList = document.createElement("ul");
+    
+    if (friends) {
+        for(let item of friends) {
+            let friend = document.createElement("li");
+            friend.textContent = `${item.firstName} ${item.lastName}`;
+            friendsList.append(friend);
+        }
+    }
+
+    return friendsList;
 }
