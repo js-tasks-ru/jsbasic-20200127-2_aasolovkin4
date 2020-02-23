3c28b50bf4953e3f7664addeb45179f00c664e64
aasolovkin <aasolovkin@eossoft.ru>
2020-02-23 04:09:29 +0300

    Решение задачи 3 занятия 6

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -26,6 +26,52 @@ class Menu {
   `;
 
   constructor(element) {
+    this.el = element;
+
+    this.el.insertAdjacentHTML("beforeEnd", this.template);
+    
+    this.el.addEventListener("click", this._menuClick.bind(this));
+
+    this.el.querySelectorAll(".list-group-item").forEach((item) => {
+      item.addEventListener("pointerenter", this._menuItemPointerEnter.bind(this));
+    });
+
+    this.el.querySelectorAll(".list-group-item").forEach((item) => {
+      item.addEventListener("pointerleave", this._menuItemPointerLeave.bind(this));
+    });
+  }
+
+  _menuClick(event) {
+    let menuItemAncor = event.target.closest("a");
+
+    if (!menuItemAncor || !this.el.contains(menuItemAncor)) return;
+
+    let menuItem = menuItemAncor.closest(".dropdown-item[data-id]");
+
+    if (!menuItem || !this.el.contains(menuItem)) return;
+
+    menuItem.dispatchEvent(new CustomEvent("select", {
+      bubbles: true,
+      detail: { id: menuItem.dataset.id }
+    }));
+  }
+
+  _menuItemPointerEnter(event) {
+    event.target.querySelector(".dropdown-menu").classList.add("show");
+
+    let backdrop = document.body.querySelector(".backdrop");
+    if (backdrop) {
+      backdrop.classList.add("show");
+    }
+  }
+
+  _menuItemPointerLeave(event) {
+    event.target.querySelector(".dropdown-menu").classList.remove("show");
+
+    let backdrop = document.body.querySelector(".backdrop");
+    if (backdrop) {
+      backdrop.classList.remove("show");
+    }
   }
 }
 
